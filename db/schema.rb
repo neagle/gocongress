62926bf0eae070f1e35694cd0bb1864ab0f21c14
compostbrain <joelcahalan@gmail.com>
2018-05-09 12:16:33 -0400

    Added round views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180506190244) do
+ActiveRecord::Schema.define(version: 20180509155300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -209,6 +209,7 @@ ActiveRecord::Schema.define(version: 20180506190244) do
     t.datetime "start_time",    null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "year"
     t.index ["tournament_id"], name: "index_rounds_on_tournament_id", using: :btree
   end
 
